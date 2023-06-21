1b5b2cb4cad301d187250704e421085bc990b241
Jonas Nygren <jonas_nygren30@hotmail.com>
2023-06-21 12:31:17 +0200

    Restart-Service upload

---
+++ Restart-Service.ps1
@@ -0,0 +1,13 @@
+﻿$serviceName = "YourServiceName"
+
+# Stop the service
+Stop-Service -Name $serviceName
+
+# Wait for a few seconds (optional)
+Start-Sleep -Seconds 5
+
+# Start the service
+Start-Service -Name $serviceName
+
+# Check the service status
+Get-Service -Name $serviceName
